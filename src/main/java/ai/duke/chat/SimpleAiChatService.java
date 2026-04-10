18f344284ad2d6414a90f834e29581dd8ec6576f
Ivar Grimstad <ivar.grimstad@eclipse-foundation.org>
2026-04-10 14:31:09 +0200

    Rename Ai Services

--- src/main/java/ai/duke/chat/SimpleAiChatService.java
+++ src/main/java/ai/duke/chat/SimpleAiChatService.java
@@ -4,9 +4,8 @@ import dev.langchain4j.cdi.spi.RegisterAIService;
 import dev.langchain4j.service.SystemMessage;
 
 @RegisterAIService
-public interface SimpleAiService {
+public interface SimpleAiChatService {
 
     @SystemMessage("You are a helpful assistant.")
     String chat(String message);
-
 }
--- src/main/java/ai/duke/chat/SimpleChatResource.java
+++ src/main/java/ai/duke/chat/SimpleChatResource.java
@@ -10,7 +10,7 @@ import jakarta.ws.rs.core.MediaType;
 public class SimpleChatResource {
 
     @Inject
-    private SimpleAiService aiService;
+    private SimpleAiChatService aiService;
 
     @GET()
     @Produces(MediaType.TEXT_PLAIN)
